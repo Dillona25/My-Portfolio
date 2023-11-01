8fade6a66d6e5b366e2b93e41622f9ee00a2912e
Dillona25 <Dillyb00by49@gmail.com>
2023-11-01 08:02:25 -0500

    Added unfinished projects section

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -2,74 +2,72 @@ import HTML from "../assets/HTML.svg";
 import CSS from "../assets/CSS.svg";
 import JS from "../assets/JS.svg";
 import NODE from "../assets/NODE.svg";
+import Us from "../images/Us.png";
 
 const ProjectCards = () => {
   return (
     <>
-      <div className="w-[450px] h-[430px] bg-white rounded-[10px]">
-        <div className="w-[450px] h-[300px] bg-gray-400 rounded-t-[10px]">
-          Image here
-        </div>
+      <div className="w-[400px] bg-white rounded-[10px] shadow-lg shadow-black">
+        <img
+          className="w-[400px] h-[220px] bg-gray-400 rounded-t-[10px]"
+          src={Us}
+        ></img>
         <div className=" flex justify-between align-middle pt-[10px] pl-[10px] pr-[10px]">
-          <h1 className="font-[Poppins] text-[25px] font-semibold mr-[10px] self-center">
+          <h1 className="font-[Poppins] text-[20px] font-semibold mr-[10px] self-center">
             Around The Us{" "}
           </h1>
-          <img className="h-[50px] w-[50px]" src={HTML}></img>
-          <img className="h-[50px] w-[50px]" src={CSS}></img>
-          <img className="h-[50px] w-[50px]" src={JS}></img>
-          <img className="h-[50px] w-[50px]" src={NODE}></img>
+          <img className="h-[45px] w-[45px]" src={HTML}></img>
+          <img className="h-[45px] w-[45px]" src={CSS}></img>
+          <img className="h-[45px] w-[45px]" src={JS}></img>
+          <img className="h-[45px] w-[45px]" src={NODE}></img>
         </div>
         <div className="flex gap-[10px] p-[10px]">
-          <button className="bg-[#9b9b84] py-[10px] px-[50px] rounded-[50px] font-[Poppins] hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
+          <button className="group relative inline-block overflow-hidden bg-[#9b9b84] py-[5px] px-[40px] rounded-[50px] font-[Poppins] hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
             Demo
           </button>
-          <button className="bg-black py-[10px] px-[50px] rounded-[50px] font-[Poppins] text-white hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
+          <button className="bg-black py-[5px] px-[40px] rounded-[50px] font-[Poppins] text-white hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
             Code
           </button>
         </div>
       </div>
 
-      <div className="w-[450px] h-[430px] bg-white rounded-[10px]">
-        <div className="w-[450px] h-[300px] bg-gray-400 rounded-t-[10px]">
-          Image here
-        </div>
+      <div className="w-[400px] bg-white rounded-[10px] shadow-lg shadow-black">
+        <div className="w-[400px] h-[220px] bg-gray-400 rounded-t-[10px]"></div>
         <div className=" flex justify-between align-middle pt-[10px] pl-[10px] pr-[10px]">
-          <h1 className="font-[Poppins] text-[25px] font-semibold mr-[10px] self-center">
+          <h1 className="font-[Poppins] text-[20px] font-semibold mr-[10px] self-center">
             Around The Us{" "}
           </h1>
-          <img className="h-[50px] w-[50px]" src={HTML}></img>
-          <img className="h-[50px] w-[50px]" src={CSS}></img>
-          <img className="h-[50px] w-[50px]" src={JS}></img>
-          <img className="h-[50px] w-[50px]" src={NODE}></img>
+          <img className="h-[45px] w-[45px]" src={HTML}></img>
+          <img className="h-[45px] w-[45px]" src={CSS}></img>
+          <img className="h-[45px] w-[45px]" src={JS}></img>
+          <img className="h-[45px] w-[45px]" src={NODE}></img>
         </div>
         <div className="flex gap-[10px] p-[10px]">
-          <button className="bg-[#9b9b84] py-[10px] px-[50px] rounded-[50px] font-[Poppins] hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
+          <button className="bg-[#9b9b84] py-[5px] px-[40px] rounded-[50px] font-[Poppins] hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
             Demo
           </button>
-          <button className="bg-black py-[10px] px-[50px] rounded-[50px] font-[Poppins] text-white hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
+          <button className="bg-black py-[5px] px-[40px] rounded-[50px] font-[Poppins] text-white hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
             Code
           </button>
         </div>
       </div>
 
-      <div className="w-[450px] h-[430px] bg-white rounded-[10px]">
-        <div className="w-[450px] h-[300px] bg-gray-400 rounded-t-[10px]">
-          Image here
-        </div>
+      <div className="w-[400px] bg-white rounded-[10px] shadow-lg shadow-black">
+        <div className="w-[400px] h-[220px] bg-gray-400 rounded-t-[10px]"></div>
         <div className=" flex justify-between align-middle pt-[10px] pl-[10px] pr-[10px]">
-          <h1 className="font-[Poppins] text-[25px] font-semibold mr-[10px] self-center">
+          <h1 className="font-[Poppins] text-[20px] font-semibold mr-[10px] self-center">
             Around The Us{" "}
           </h1>
-          <img className="h-[50px] w-[50px]" src={HTML}></img>
-          <img className="h-[50px] w-[50px]" src={CSS}></img>
-          <img className="h-[50px] w-[50px]" src={JS}></img>
-          <img className="h-[50px] w-[50px]" src={NODE}></img>
+          <img className="h-[45px] w-[45px]" src={HTML}></img>
+          <img className="h-[45px] w-[45px]" src={CSS}></img>
+          <img className="h-[45px] w-[45px]" src={JS}></img>
+          <img className="h-[45px] w-[45px]" src={NODE}></img>
         </div>
         <div className="flex gap-[10px] p-[10px]">
-          <button className="bg-[#9b9b84] py-[10px] px-[50px] rounded-[50px] font-[Poppins] hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
+          <button className="bg-[#9b9b84] py-[5px] px-[40px] rounded-[50px] font-[Poppins] hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
             Demo
           </button>
-          <button className="bg-black py-[10px] px-[50px] rounded-[50px] font-[Poppins] text-white hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
+          <button className="bg-black py-[5px] px-[40px] rounded-[50px] font-[Poppins] text-white hover:opacity-80 hover:scale-105 ease-in-out duration-[.5s]">
             Code
           </button>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,14 +2,22 @@ import ProjectCards from "./ProjectCards";
 
 const Projects = () => {
   return (
-    <div className=" max-w-[100%] mt-[150px] mx-[70px]">
-      <h1 className="font-[Poppins] text-white text-[50px] font-semibold pb-[10px] border-[#f5dfa1] border-b-2">
+    <div className=" max-w-[100%] mt-[150px] pb-[125px] bg-gradient-to-b to-white from-black ">
+      <h1 className="font-[Poppins] text-white text-[50px] font-semibold pb-[10px] border-[#f5dfa1] border-b-2 mx-[70px]">
         Projects
       </h1>
-      <div className="flex flex-wrap align-middle max-w-[100%] justify-center gap-[30px] mt-[100px]">
+      <div className="flex flex-wrap align-middle max-w-[100%] justify-center gap-[30px] mt-[125px] mx-[70px]">
         <ProjectCards />
       </div>
-      <button className="mt-[150px]">See More</button>
+      <div className="flex justify-center">
+        <button className="group relative inline-block overflow-hidden rounded border border-gray-100 bg-black  px-12 py-4 text-sm font-medium mt-[75px] text-white hover:text-[#f5dfa1] focus:outline-none focus:ring active:bg-[#f5dfa1] active:text-white">
+          <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-[#f5dfa1] transition-all duration-[1s] group-hover:w-full"></span>
+          <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-[#f5dfa1] transition-all duration-[1s] group-hover:h-full"></span>
+          <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-[#f5dfa1] transition-all duration-[1s] group-hover:w-full"></span>
+          <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-[#f5dfa1] transition-all duration-[1s] group-hover:h-full"></span>
+          See more
+        </button>
+      </div>
     </div>
   );
 };
