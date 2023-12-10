1af9017d3c2d79a2f5176603d907651950933521
Dillona25 <Dillyb00by49@gmail.com>
2023-12-09 23:45:42 -0600

    Updated bugs

--- src/components/Projects/SimplyDoIt.jsx
+++ src/components/Projects/SimplyDoIt.jsx
@@ -26,7 +26,7 @@ const SimplyDoIt = ({ showCard, category }) => {
           className="hidden justify-center lg:flex rounded-lg w-[100px] top-[-70px] position: relative shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]"
         ></img>
       </div>
-      <div className="flex flex-col justify-between h-[330px] bg-white w-full mt-[-50px] p-[10px] rounded-lg">
+      <div className="flex flex-col gap-[20px] justify-between h-fit bg-white w-full mt-[-50px] p-[10px] rounded-lg">
         <div className="flex gap-[10px] w-full border-b-[2px] border-black border-opacity-50">
           <h1 className="font-[Poppins] font-semibold text-black text-[20px] md:text-[25px]">
             Simply Do It
